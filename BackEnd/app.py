50d3c5e6c278bf2d1b94ed5b4ae97c79699098be
Jordan Gillispie <jwgillispie01@gmail.com>
2023-04-14 17:54:10 -0400

    more cors

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -2,11 +2,10 @@ from flask import Flask, request, jsonify
 from graph import User, Cluster, EntirePopulation
 from flask_cors import CORS, cross_origin
 
-
-
-
 app = Flask(__name__)
 cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 cluster = Cluster("Default Cluster")
 pop = EntirePopulation()
